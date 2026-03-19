d8f913fc8adb9bdda5782dab37d497f01cf7df38
estymagb <estymagb@mit.edu>
2026-03-19 01:32:40 -0400

    Step 0 prereq

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -39,6 +39,7 @@
         {url: "/projects", title: "Projects"},
         {url: "/resume", title: "Resume"},
         {url: "/contact", title: "Contact"},
+        {url: "/meta", title: "Meta"},
         {url: "https://github.com/estymagb", title: "Github"},
         // add other pages here
     ];
