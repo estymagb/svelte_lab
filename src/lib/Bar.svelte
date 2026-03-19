d078212a452831984016ce5c04699f41ad3c729b
estymagb <estymagb@mit.edu>
2026-03-19 10:45:21 -0400

    Make bar chart

---
+++ src/lib/Bar.svelte
@@ -0,0 +1,95 @@
+<p>Hello from Bar.svelte</p>
+<script>
+    import * as d3 from 'd3';
+    let width = 400;
+    let height = 300;
+
+    let data = [
+        { label: "A", value: 10 },
+        { label: "B", value: 20 },
+        { label: "C", value: 15 },
+        { label: "D", value: 8 },
+        { label: "E", value: 25 },
+    ];
+
+    // Adding color/bar chart
+    let margin = { top:20, right: 20, bottom: 30, left:60 };
+    let innerWidth = width - margin.left - margin.right;
+    let innerHeight = height - margin.top - margin.bottom;
+
+    $: xScale = d3.scaleBand()
+        .domain(data.map(d => d.label))
+        .range([0, innerWidth])
+        .padding(0.2);
+
+    $: yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d=> d.value) || 1])
+        .range([innerHeight, 0]);
+
+    $: colorScale = d3.scaleOrdinal(d3.schemeTableau10)
+        .domain(data.map(d => d.label))
+
+    $: if (xAxis && yAxis) {
+        d3.select(xAxis).call(d3.axisBottom(xScale));
+        d3.select(yAxis).call(d3.axisLeft(yScale));
+    }
+    // Adding axes
+    let xAxis, yAxis;
+
+</script>
+<div class="container">
+    <svg viewBox="0 0 {width} {height}">
+        <g transform="translate({margin.left}, {margin.top + innerHeight})"
+        bind:this={xAxis} />
+        <g transform="translate({margin.left}, {margin.top})"
+        bind:this={yAxis} />
+        <g transform="translate({margin.left}, {margin.top})">
+            {#each data as d}
+            <rect
+                x={xScale(d.label)}
+                y={yScale(d.value)}
+                width={xScale.bandwidth()}
+                height={innerHeight - yScale(d.value)}
+                fill={colorScale(d.label)}
+                />
+            {/each}
+        </g>
+    </svg>
+    <ul class="legend">
+        {#each data as d}
+            <li style="--color: {colorScale(d.label)}">
+                <span class="swatch"></span>
+                {d.label}<em>({d.value})</em>
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<style>
+    svg {
+        max-width: 100%;
+        height: auto;
+        overflow: visible;
+    }
+
+    .container {
+        display: flex;
+    }
+    .legend {
+        flex: 1;
+    }
+
+    .swatch {
+        width: 20px;
+        height: 20px;
+        background-color: var(--color);
+
+    }
+
+    li {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+
+    }
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,24 +2,36 @@
     import projects from "$lib/projects.json";
     import ProjectNarrative from "../../lib/ProjectNarrative.svelte";
     import Project from "$lib/Project.svelte";
+    import Bar from '$lib/Bar.svelte';
     import {onMount} from 'svelte';
     import * as d3 from 'd3';
-
-    let rawData = [];
 
     let years = projects.map(proj => proj.year);
     let range = Math.max(...years) - Math.min(...years);
 
+    let rawData = [];
+    let wrangled = [];
     onMount(async() => {
         rawData = await d3.json('/lab6_example.json');
-        console.log(rawData);
-    })
+        wrangled = d3.rollups(
+            rawData,
+            v => d3.sum(v, d=> d.lines),
+            d => d.language
+        );
+    });
+
 </script>
 <svelte:head>
   <title> Projects</title>
 </svelte:head>
 
 <h1>{projects.length} Projects over {range} Years</h1>
+
+<section>
+    <h2>Data wrangling result</h2>
+    <pre>{JSON.stringify(wrangled, null, 2)}</pre>
+    <Bar/>
+</section>
 
 <p>Scroll down to see my a timeline of my projects and how I've learned from them.</p>
 
